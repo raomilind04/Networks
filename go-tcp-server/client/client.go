08906468bd08754761fd729b64327423ced5bdf5
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2024-03-08 23:01:03 +0530

    add chan to handle server failure

--- go-tcp-server/client/client.go
+++ go-tcp-server/client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"strings"
@@ -14,16 +15,26 @@ func main() {
         fmt.Println(err)
         return 
     }
-    go handleServerEvents(conn)
-    handleUserMessages(conn)
+    defer conn.Close()
+
+    terminate := make(chan bool)
+
+    go handleServerEvents(conn, terminate)
+    handleUserMessages(conn, terminate)
 
 }
 
-func handleServerEvents(conn net.Conn) {
+func handleServerEvents(conn net.Conn, terminate chan bool) {
     buffer := make([]byte, 1024)
     for {
         bytesReceived, err := conn.Read(buffer)
         if err != nil {
+            if err == io.EOF {
+                fmt.Println("The server has disconnected") 
+                terminate <- true
+                fmt.Println("Terminate siganl sent")
+                return
+            }
             fmt.Println(err)
         }
         if bytesReceived > 0 {
@@ -32,31 +43,39 @@ func handleServerEvents(conn net.Conn) {
     }
 }
 
-func handleUserMessages(conn net.Conn) {
+func handleUserMessages(conn net.Conn, terminate chan bool) {
     var userName string
     fmt.Print("Please enter your name: ")
     fmt.Scanln(&userName)
-    
-
     fmt.Println("Enter you message: ")
     
     reader := bufio.NewReader(os.Stdin)
 
     for {
-        var msg string
-        msg, err := reader.ReadString('\n')
-        if err != nil {
-            fmt.Println(err)
+        select {
+        case <-terminate:
+            fmt.Println("Sever disconnected")
             return 
-        }
-        
-        msg = strings.TrimSpace(msg)
+        default:
+            var msg string
+            msg, err := reader.ReadString('\n')
+            if err != nil {
+                fmt.Println(err)
+                return 
+            }
+            
+            msg = strings.TrimSpace(msg)
 
-        if strings.ToUpper(msg) == "EXIT" {
-            break
+            if strings.ToUpper(msg) == "EXIT" {
+                break
+            }
+
+            formattedMsg := fmt.Sprintf("%s: %s", userName, msg)
+            _, err = conn.Write([]byte(formattedMsg))
+            if err != nil {
+                fmt.Println(err)
+                return
+            }
         }
-        formattedMsg := fmt.Sprintf("%s: %s", userName, msg)
-        conn.Write([]byte(formattedMsg))
     }
-       
 }
