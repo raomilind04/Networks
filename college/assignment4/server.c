53c14ee9fbce06872bb96df5d1ba51a5f5c905fc
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2024-03-31 17:54:05 +0530

    make the server multithreaded

--- college/assignment4/server.c
+++ college/assignment4/server.c
@@ -5,25 +5,32 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
-struct sockaddr_in* createTCPServerAddress(char* input_ip, int port); 
-void handleClient(int client_FD, struct sockaddr_in client);
-int getUserInput(); 
+struct sockaddr_in* createTCPServerAddress(char* input_ip, int port);
+void* handleClient(void* arg);
+int getUserInput();
+
+typedef struct {
+    int client_FD;
+    struct sockaddr_in client_addr;
+} client_info_t;
+
+int server_constant; 
 
 int main(int argc, char *argv[]) {
     // Get ip and port from user defined options
     int opt;
-    char* input_ip= NULL; 
-    int port = -1; 
-    int server_constant; 
-    while((opt = getopt(argc, argv, "i:p:c:")) != -1) {
-        switch(opt) {
+    char* input_ip = NULL;
+    int port = -1;
+    while ((opt = getopt(argc, argv, "i:p:c:")) != -1) {
+        switch (opt) {
             case 'i':
                 input_ip = optarg;
-                break; 
+                break;
             case 'p':
-                port = atoi(optarg); 
-                break; 
+                port = atoi(optarg);
+                break;
             case 'c':
                 server_constant = atoi(optarg); 
                 break; 
@@ -31,84 +38,89 @@ int main(int argc, char *argv[]) {
                 perror("Invalid args");
                 exit(1);
         }
-
     }
+
     if (input_ip == NULL || port == -1) {
-        perror("Missing args"); 
-        exit(1); 
+        perror("Missing args");
+        exit(1);
     }
-    
-    printf("User Args -> \n"); 
-    printf("Server Address: %s\n", input_ip); 
-    printf("Server Port: %d\n", port); 
-    printf("Server Constant: %d\n", server_constant);  
 
-    //Create file descriptor and server adder struct
-    int server_FD = socket(AF_INET, SOCK_STREAM, 0); 
+    printf("User Args -> \n");
+    printf("Server Address: %s\n", input_ip);
+    printf("Server Port: %d\n", port);
+
+    // Create file descriptor and server adder struct
+    int server_FD = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in* server_addr = createTCPServerAddress(input_ip, port);
 
     // Bind
-    int binding_result = bind(server_FD, (struct sockaddr*) server_addr, sizeof(*server_addr)); 
+    int binding_result = bind(server_FD, (struct sockaddr*)server_addr, sizeof(*server_addr));
     if (binding_result == 0) {
-        printf("[Binding Completed] ip: %s | port: %d\n", input_ip, port); 
+        printf("[Binding Completed] ip: %s | port: %d\n", input_ip, port);
     } else {
-        perror("[Binding] Failed\n"); 
+        perror("[Binding] Failed\n");
     }
 
     // Listen
-    int listen_result = listen(server_FD, 10); 
+    int listen_result = listen(server_FD, 10);
     if (listen_result == 0) {
-        printf("[Listen] Listening...\n"); 
+        printf("[Listen] Listening...\n");
     } else {
-        perror("[Listen] Failed\n"); 
+        perror("[Listen] Failed\n");
     }
 
-
-    // Accept 
+    // Accept
     while (1) {
-        struct sockaddr_in client_addr;  
-        socklen_t client_addr_size = sizeof(struct sockaddr_in); 
-        int client_FD = accept(server_FD, (struct sockaddr*) &client_addr, &client_addr_size); 
+        struct sockaddr_in client_addr;
+        socklen_t client_addr_size = sizeof(struct sockaddr_in);
+        int client_FD = accept(server_FD, (struct sockaddr*)&client_addr, &client_addr_size);
         if (client_FD <= 0) {
-            perror("[Accept] Unable to accept client"); 
+            perror("[Accept] Unable to accept client");
         }
-        
-        // Receive  
-        handleClient(client_FD, client_addr); 
+
+        // Create a new thread to handle the client
+        client_info_t* client_info = malloc(sizeof(client_info_t));
+        client_info->client_FD = client_FD;
+        client_info->client_addr = client_addr;
+        pthread_t thread;
+        pthread_create(&thread, NULL, handleClient, (void*)client_info);
+        pthread_detach(thread);
     }
 
-    // shutdown the server
+    // Shutdown the server
     shutdown(server_FD, SHUT_RDWR);
-    
-    return 0; 
+    return 0;
 }
 
 struct sockaddr_in* createTCPServerAddress(char* input_ip, int port) {
-    struct sockaddr_in* server_addr = malloc(sizeof(struct sockaddr_in)); 
+    struct sockaddr_in* server_addr = malloc(sizeof(struct sockaddr_in));
     server_addr->sin_family = AF_INET;
-    server_addr->sin_port = htons(port); 
-    inet_pton(AF_INET, input_ip, &server_addr->sin_addr.s_addr); 
-    return server_addr; 
+    server_addr->sin_port = htons(port);
+    inet_pton(AF_INET, input_ip, &server_addr->sin_addr.s_addr);
+    return server_addr;
 }
 
-void handleClient(int client_FD, struct sockaddr_in client) {
-    int client_msg; 
-    ssize_t client_msg_size = recv(client_FD, &client_msg, sizeof(client_msg), 0);
+void* handleClient(void* arg) {
+    client_info_t* client_info = (client_info_t*)arg;
+    int client_FD = client_info->client_FD;
+    struct sockaddr_in client = client_info->client_addr;
+    free(client_info); // Free the client_info struct
 
+    int client_msg;
+    ssize_t client_msg_size = recv(client_FD, &client_msg, sizeof(client_msg), 0);
     if (client_msg_size > 0) {
         // Receive client_msg
         printf("Number from client is %d\n", client_msg);
-        
         // Get user input at server end
-        int server_msg = server_constant + client_msg; 
-
+        int server_msg = server_constant + client_msg;
         // Send sum of server_msg and client_msg back
-        send(client_FD, &server_msg, sizeof(server_msg), 0); 
+        send(client_FD, &server_msg, sizeof(server_msg), 0);
     } else if (client_msg_size == 0) {
-       printf("client_msg_size was 0\n");  
+        printf("client_msg_size was 0\n");
     }
 
     // Close the client FD
-    close(client_FD); 
+    close(client_FD);
+    pthread_exit(NULL);
 }
 
